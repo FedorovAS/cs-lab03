7f4ee3eaf55771a21f209f2e9d6cbade3037c108
FedorovAS <mrus@inbox.ru>
2019-04-16 07:54:53 +0300

    code: Модульные тесты

---
+++ lab03-test/main.cpp
@@ -0,0 +1,56 @@
+#include "histogram.h"
+
+#include <cassert>
+
+void
+test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void
+test_negativ() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void
+test_odinakov() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void
+test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void
+test_pust() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+int main()
+{
+       test_positive();
+       test_negativ();
+       test_odinakov();
+       test_one();
+       test_pust();
+}
